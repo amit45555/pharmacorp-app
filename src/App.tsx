cbad517a2a4ff45dead62da961691c135a58dfd7
kri-sketch <76882363+kri-sketch@users.noreply.github.com>
2025-08-21 14:44:02 +0530

    changes for vercel insight

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from "react";
 import "./App.module.css";
+import { BrowserRouter } from "react-router-dom";
+
 import { FormProvider } from "./context/FormContext";
 import { AuthProvider } from "./context/AuthContext";
 import { RolesProvider } from "./RoleMaster/RolesContext";
 import { UserProvider } from "./context/UserContext";
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
 import { ApplicationsProvider } from "./context/ApplicationsContext";
+
+import AppRoutes from "./routes/AppRoutes";
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
-      <SpeedInsights />
       <FormProvider>
         <RolesProvider>
           <UserProvider>
-            <BrowserRouter>
-              <ApplicationsProvider>
+            <ApplicationsProvider>
+              <BrowserRouter>
+                {/* Main App Routes */}
                 <AppRoutes />
-              </ApplicationsProvider>
-            </BrowserRouter>
+
+                {/* Vercel Integrations */}
+                <SpeedInsights />
+                <Analytics />
+              </BrowserRouter>
+            </ApplicationsProvider>
           </UserProvider>
         </RolesProvider>
       </FormProvider>
